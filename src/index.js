0aa81c2cbbab8bbcdb0c49261489cb258cfa99b3
cratuss <ldoh92@gmail.com>
2022-10-06 21:06:33 +0900

    Add: 배포정보

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./pages/Router";
+import { HashRouter } from "react-router-dom";
 
 import "./styles/index.scss";
 import "./styles/reset.scss";
@@ -8,6 +9,8 @@ import "./styles/reset.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Router />
+    <HashRouter>
+      <Router />
+    </HashRouter>
   </>
 );
